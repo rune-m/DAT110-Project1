c8c5071beb1852143576bf81a719640e8f96ea87
Rune <rune.maestad@gmail.com>
2021-01-26 14:34:15 +0100

    system.controller finished

--- dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
@@ -4,6 +4,8 @@ import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.RPCClient;
 import no.hvl.dat110.rpc.RPCServerStopStub;
 
+import java.util.Objects;
+
 public class Controller  {
 	
 	private static int N = 5;
@@ -28,20 +30,26 @@ public class Controller  {
 		// create local display and sensor objects
 		// register display and sensor objects in the RPC layer
 		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
-		
+		sensorclient.connect();
+		displayclient.connect();
+
+		display = new Display();
+		sensor = new Sensor();
+
+		display.register(displayclient);
+		sensor.register(sensorclient);
+
 		// register stop methods in the RPC layer
 		displayclient.register(stopdisplay);
 		sensorclient.register(stopsensor);
 		
 		// TODO:
 		// loop while reading from sensor and write to display via RPC
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-			}
+
+		for (int i = 0; i < N; i++) {
+			int temp = sensor.read();
+			display.write(Integer.toString(temp));
+		}
 		
 		stopdisplay.stop();
 		stopsensor.stop();
--- dat110-project1/src/no/hvl/dat110/system/controller/Display.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Display.java
@@ -12,6 +12,10 @@ public class Display extends RPCStub {
 		// TODO
 		// implement marshalling, call and unmarshalling for write RPC method
 
-		throw new UnsupportedOperationException(TODO.method());
+		byte[] request = RPCUtils.marshallString(RPCID, message);
+		byte[] response = rpcclient.call(request);
+
+		RPCUtils.unmarshallVoid(response);
+
 	}
 }
